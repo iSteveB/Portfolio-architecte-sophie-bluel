6e636de82f84add66c2c43d6ecf2603a6dba4bfe
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-05-04 12:07:43 +0200

    feat: add login feature

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,3 +1,26 @@
+import { login } from './services/login-service.js';
 
+const loginForm = document.querySelector('#login form');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
 
-
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const email = emailInput.value;
+  const password = passwordInput.value;
+  
+  login(email, password)
+    .then(({token, userId}) => {
+      if (!token || !userId) {
+        alert("ERREUR : Impossible de se connecter");
+      } else {
+        localStorage.setItem('token', token);
+        localStorage.setItem('userId', userId);
+        window.location.href = 'index.html';
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+})
