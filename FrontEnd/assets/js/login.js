b42f7666b3ef196ebcf751720887120e483d8e43
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-05-04 12:46:36 +0200

    feat: add error message for wrong  email/password

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -3,6 +3,7 @@ import { login } from './services/login-service.js';
 const loginForm = document.querySelector('#login form');
 const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
+const error = document.querySelector('.error');
 
 loginForm.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -13,11 +14,11 @@ loginForm.addEventListener('submit', (event) => {
   login(email, password)
     .then(({token, userId}) => {
       if (!token || !userId) {
-        alert("ERREUR : Impossible de se connecter");
+        error.textContent = "Erreur dans l’identifiant ou le mot de passe";
       } else {
         localStorage.setItem('token', token);
         localStorage.setItem('userId', userId);
-        window.location.href = 'index.html';
+        window.location.href = '../../index.html';
       }
     })
     .catch((error) => {
