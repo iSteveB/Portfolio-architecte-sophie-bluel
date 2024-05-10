2e5d948de21e96ad134d90511d79280dda67d6d6
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-05-10 11:19:49 +0200

    feat: adpat createElement factory for input element

--- FrontEnd/assets/js/models/htmlElement-model.js
+++ FrontEnd/assets/js/models/htmlElement-model.js
@@ -1,8 +1,14 @@
-export const createElement = (tag, className, textContent) => {
-	const el = document.createElement(tag);
-	if (className) el.className = className;
-	if (textContent) el.textContent = textContent;
-	return el;
+export const createElement = (tag, className, textContent, innerHTML, attributes) => {
+	const element = document.createElement(tag);
+	if (className) element.className = className;
+	if (textContent) element.textContent = textContent;
+	if (innerHTML) element.innerHTML = innerHTML;
+	if (attributes) {
+			for (const key in attributes) {
+					element.setAttribute(key, attributes[key]);
+			}
+	}
+	return element;
 }
 
 export const createListItem = (item) => {
