3c46552a07d0839e9d794b1b152c947f3d311fce
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-05-10 11:12:50 +0200

    refactor: use factory to create html elements

--- FrontEnd/assets/js/components/form-component.js
+++ FrontEnd/assets/js/components/form-component.js
@@ -1,189 +1,62 @@
-export const createWorkModal = () => {
+import { createElement } from '../models/htmlElement-model.js';
+import { previewImage, sendForm } from '../models/form-model.js';
+
+export const createFormModal = () => {
   const modal = document.querySelector('.modal');
-  const form = createModalForm();
+  const h2 = createElement('h2', null, 'Ajouter une photo');
+  const closeModal = createElement('span', 'close-modal', null, '&#215;', );
+  const goBackButton = createElement('span', 'go-back', null, '&#8592;', );
+  const form = createElement('form', 'modal-form');
+  const formContainer = createElement('div', 'form-container');
+  const uploadFileContainer = createElement('div', 'upload-file-container');
+  const fileLabel = createElement('label', 'upload-file');
+  const fileImg = createElement('img', null, null, null, { src: './assets/icons/image-icon.svg', alt: 'upload' });
+  const fileText = createElement('p', null, '+ Ajouter photo');
+  const fileSpan = createElement('span', null, 'jpg, png : 4mo max');
+  const fileInput = createElement('input', null, null, null, { type: 'file', name: 'file', id: 'file', required: true, accept: 'image/jpg, image/png' });
+  const inputContainer = createElement('div', 'input-container');
+  const titleContainer = createElement('div', 'title-container');
+  const titleLabel = createElement('label', null, 'Titre');
+  const titleInput = createElement('input', null, null, null, { type: 'text', name: 'title', id: 'title', required: true });
+  const categoryContainer = createElement('div', 'category-container');
+  const categoryLabel = createElement('label', null, 'Categorie');
+  const categorySelect = createElement('select', null, null, null, { name: 'category', id: 'category' });
+  const option1 = createElement('option', null, 'Objet', null, { value: '1' });
+  const option2 = createElement('option', null, 'Appartement', null, { value: '2' });
+  const option3 = createElement('option', null, 'Hotels & restaurants', null, { value: '3' });
+  const submitBtn = createElement('button', null, 'Valider', null, { type: 'submit' });
+
+
   modal.innerHTML = '';
+  modal.appendChild(h2);
+  modal.appendChild(closeModal);
+  modal.appendChild(goBackButton);
   modal.appendChild(form);
+  form.appendChild(formContainer);
+  formContainer.appendChild(uploadFileContainer);
+  uploadFileContainer.appendChild(fileLabel);
+  fileLabel.appendChild(fileImg);
+  fileLabel.appendChild(fileText);
+  fileLabel.appendChild(fileSpan);
+  fileLabel.appendChild(fileInput);
+  formContainer.appendChild(inputContainer);
+  inputContainer.appendChild(titleContainer);
+  titleContainer.appendChild(titleLabel);
+  titleContainer.appendChild(titleInput);
+  inputContainer.appendChild(categoryContainer);
+  categoryContainer.appendChild(categoryLabel);
+  categoryContainer.appendChild(categorySelect);
+  categorySelect.appendChild(option1);
+  categorySelect.appendChild(option2);
+  categorySelect.appendChild(option3);
+  form.appendChild(submitBtn);
 
-  return form;
-};
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    sendForm();
+  })
+  previewImage(fileInput);
 
-const createModalForm = () => {
-  const form = document.createElement('form');
-  form.classList.add('modal-form');
-  form.appendChild(createFormContainer());
-  form.appendChild(createSubmitButton('submit', 'Valider'));
-
-  return form;
-};
-
-const createFormContainer = () => {
-  const container = document.createElement('div');
-  container.classList.add('form-container');
-  container.appendChild(createUploadFileContainer());
-  container.appendChild(createInputContainer());
-
-  return container;
-};
-
-const createUploadFileContainer = () => {
-  const container = document.createElement('div');
-  container.classList.add('upload-file-container');
-  container.appendChild(createUploadFileLabel());
-  container.appendChild(createUploadFileInput());
-
-  return container;
-};
-
-const createUploadFileLabel = () => {
-  const label = document.createElement('label');
-  label.htmlFor = 'file';
-  label.classList.add('upload-file');
-  label.appendChild(createUploadFileIcon());
-  label.appendChild(createUploadFileText());
-  label.appendChild(createUploadFileDescription());
-
-  return label;
-};
-
-const createUploadFileIcon = () => {
-  const icon = document.createElement('img');
-  icon.src = './assets/icons/image-icon.svg';
-  icon.alt = 'upload';
-
-  return icon;
-};
-
-const createUploadFileText = () => {
-  const text = document.createElement('p');
-  text.textContent = '+ Ajouter photo';
-
-  return text;
-};
-
-const createUploadFileDescription = () => {
-  const description = document.createElement('span');
-  description.textContent = 'jpg, png : 4mo max';
-
-  return description;
-};
-
-const createUploadFileInput = () => {
-  const input = document.createElement('input');
-  input.type = 'file';
-  input.name = 'file';
-  input.id = 'file';
-  input.required = true;
-  input.accept = 'image/jpg, image/png';
-  input.style.display = 'none';
-
-  return input;
-};
-
-const createInputContainer = () => {
-  const container = document.createElement('div');
-  container.classList.add('input-container');
-  container.appendChild(createTitleContainer());
-  container.appendChild(createCategoryContainer());
-
-  return container;
-};
-
-const createTitleContainer = () => {
-  const container = document.createElement('div');
-  container.classList.add('title-container');
-  container.appendChild(createTitleLabel());
-  container.appendChild(createTitleInput());
-
-  return container;
-};
-
-const createTitleLabel = () => {
-  const label = document.createElement('label');
-  label.htmlFor = 'title';
-  label.textContent = 'Titre';
-
-  return label;
-};
-
-const createTitleInput = () => {
-  const input = document.createElement('input');
-  input.type = 'text';
-  input.name = 'title';
-  input.id = 'title';
-  input.required = true;
-
-  return input;
-};
-
-const createCategoryContainer = () => {
-  const container = document.createElement('div');
-  container.classList.add('category-container');
-  container.appendChild(createCategoryLabel());
-  container.appendChild(createCategorySelect());
-
-  return container;
-};
-
-const createCategoryLabel = () => {
-  const label = document.createElement('label');
-  label.htmlFor = 'category';
-  label.textContent = 'Categorie';
-
-  return label;
-};
-
-const createCategorySelect = () => {
-  const select = document.createElement('select');
-  select.name = 'category';
-  select.id = 'category';
-  createCategoryOptions(select);
-
-  return select;
-};
-
-const createCategoryOptions = (select) => {
-  const options = [
-    { value: '1', text: 'Objet' },
-    { value: '2', text: 'Appartement' },
-    { value: '3', text: 'Hotels & restaurants' },
-  ];
-
-  options.forEach(option => {
-    const optionElement = createCategoryOption(option.value, option.text);
-    select.appendChild(optionElement);
-  });
-};
-
-const createCategoryOption = (value, text) => {
-  const option = document.createElement('option');
-  option.value = value;
-  option.textContent = text;
-
-  return option;
-};
-
-const createSubmitButton = (type, value) => {
-  const button = document.createElement('button');
-  button.type = type;
-  button.textContent = value;
-  button.disabled = true;
-
-  return button;
-};
-
-import { checkUser } from '../utils/checkUser.js';
-import { createWork } from '../services/work-service.js';
-
-const handleFormSubmit = (event) => {
-  event.preventDefault();
-
-  const formData = new FormData(event.target);
-  const title = formData.get('title');
-  const category = formData.get('category');
-  const file = formData.get('file');
-
-  if (title && category && file) {
-    const token = checkUser();
-    createWork(title, category, file, token);
-  }
+  return modal;
 }
+
