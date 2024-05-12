041b60405aa756ed4031ed08d8e5d87b5cc3dc89
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-05-12 19:26:57 +0200

    fix: preview image size and input size

--- FrontEnd/assets/js/models/form-model.js
+++ FrontEnd/assets/js/models/form-model.js
@@ -1,13 +1,20 @@
 import { checkUser } from '../utils/checkUser.js';
 import { createWork } from '../services/work-service.js';
+import { createElement } from './htmlElement-model.js';
 
 export const previewImage = (input) => {
 	input.addEventListener('change', (event) => {
 		const file = event.target.files[0];
 		const reader = new FileReader();
 		reader.onload = function (event) {
-			const img = document.querySelector('.upload-file img');
-			img.src = event.target.result;
+			const label = document.querySelector('.upload-file');
+			const previewContainer = createElement('div', 'preview-container');
+			const previewImage = createElement('img', 'preview-image');
+			
+			previewImage.src = event.target.result;
+			label.innerHTML = '';
+			previewContainer.appendChild(previewImage);
+			label.appendChild(previewContainer);
 		};
 		reader.readAsDataURL(file);
 	});
